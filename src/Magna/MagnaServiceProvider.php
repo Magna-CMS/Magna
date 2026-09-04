6f17b047119b0487eb4e65834526b5e90bbcadba
Jishnu <jishnurajm@gmail.com>
2026-09-04 20:09:46 +0530

    chore(release): 1.3.24

--- src/Magna/MagnaServiceProvider.php
+++ src/Magna/MagnaServiceProvider.php
@@ -34,7 +34,7 @@ use Magna\Webhooks\WebhookServiceProvider;
  */
 class MagnaServiceProvider extends ServiceProvider
 {
-    public const VERSION = '1.3.23';
+    public const VERSION = '1.3.24';
 
     public function register(): void
     {
